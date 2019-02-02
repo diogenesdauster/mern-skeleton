b681c12c2f5c7505c1c30e5dba8a7031bc86204f
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-01 21:25:53 -0300

    feat: create a new template html for requests in /

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+  return `<!doctype html>
+  <html land="en">
+    <head>
+      <meta charset="utf-8">
+      <title>MERN Skeleton</title>
+    </head>
+    <body>
+      <div if ="root">Hello World</div>
+    </body>
+  </html>`
+}
