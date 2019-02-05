b7cb943bb2199edf4e31669f7c013efece0373f9
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-04 21:23:22 -0300

    chore: install react library

--- template.js
+++ template.js
@@ -4,6 +4,8 @@ export default () => {
     <head>
       <meta charset="utf-8">
       <title>MERN Skeleton</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     </head>
     <body>
     <div id="root"></div><script type="text/javascript" src="/dist/bundle.js"></script>
