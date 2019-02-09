79e5bc22c84dd21e365c0aafa07206c29493fb09
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-09 16:37:08 -0300

    chore: update the configuration of babel/webpack change the configuration of client and client.production, because were exchanged. And update the babel with @babel-env-react and hot library

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -4,26 +4,40 @@ const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
-  mode: "production",
-  entry:[
-    path.join(CURRENT_WORKING_DIR, "client/main.js")
-  ],
-  output: {
-    path: path.join(CURRENT_WORKING_DIR, "/dist"),
-    filname: "bundle.js",
-    publicPath: "/dist/"
-  },
-  module:{
-    rules:[
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use:[
-          "babel-loader"
+    name: "browser",
+    mode: "development",
+    devtool: 'eval-source-map',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?reload=true',
+        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+    ],
+    output: {
+        path: path.join(CURRENT_WORKING_DIR , '/dist/'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+                use: 'file-loader'
+            }
         ]
-      }
-    ]
-  }
- }
+    },  plugins: [
+
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      // Use NoErrorsPlugin for webpack 1.x
+      new webpack.NoEmitOnErrorsPlugin()
+      ]
+}
 
 module.exports = config
